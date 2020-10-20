fddc959c60b8b193a3c5776e22893073fd881f27
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2020-10-20 17:09:43 -0400

    Setting up the project, added basic styles

---
+++ src/components/addItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddItem = () => {
+    return (
+        <div className="addItemContainer">
+            <input type="text"/>
+            <button class="addButton">Add New Item</button>
+        </div>
+    )
+}
+
+export default AddItem
